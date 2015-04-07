599a57a94393e345ad9ea8e140099844a46153f3
PhilipHolzhueter <philiphowa@web.de>
2015-04-07 16:47:34 +0200

    first xml syntax check

---
+++ src/xmlparse/MyErrorHandler.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmlparse;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ *
+ * @author Philips
+ */
+public class MyErrorHandler implements ErrorHandler {
+    public void warning(SAXParseException e) throws SAXException {
+        System.out.println(e.getMessage());
+    }
+
+    public void error(SAXParseException e) throws SAXException {
+        System.out.println(e.getMessage());
+    }
+
+    public void fatalError(SAXParseException e) throws SAXException {
+        System.out.println(e.getMessage());
+    }
+}
