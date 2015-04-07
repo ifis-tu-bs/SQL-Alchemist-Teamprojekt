35bcc6f507d91e796022e9bfa859f565e70cbf82
Runge93 <tobiasrunge1@web.de>
2015-04-07 16:10:44 +0200

    erste Version zum parsen eines teils

--- src/xmlparse/Relation.java
+++ src/xmlparse/Relation.java
@@ -11,9 +11,16 @@ package xmlparse;
 public class Relation {
 
     private String intension;
-    private String tuple;
+    private String[] tuple;
 
     public Relation() {
+        this.tuple = new String[0];
+
+    }
+
+    public Relation(String intension, String[] tuple) {
+        this.intension = intension;
+        this.tuple = tuple;
     }
 
     public String getIntension() {
@@ -25,10 +32,29 @@ public class Relation {
     }
 
     public String getTuple() {
-        return tuple;
+        String s = "";
+        for (int i = 0; i < this.tuple.length; i++) {
+
+            s += this.tuple[i] + "\n";
+        }
+        return s;
     }
 
-    public void setTuple(String tuble) {
-        this.tuple = tuple;
+    public void setTuple(String tuple) {
+        String[] temp = new String[this.tuple.length + 1];
+        for (int i = 0; i < this.tuple.length; i++) {
+            temp[i] = this.tuple[i];
+        }
+        temp[temp.length - 1] = tuple;
+        this.tuple = temp;
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Relation \n");
+        sb.append("Intension:" + getIntension() + "\n");
+        sb.append("Tuple:" + getTuple() + "\n");
+
+        return sb.toString();
     }
 }
---
+++ src/xmlparse/SAXParserExample.java
@@ -0,0 +1,115 @@
+package xmlparse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXParserExample extends DefaultHandler{
+
+	List myrelation;
+        List myheader;
+        List mytask;
+	
+	private String tempVal;
+	
+	//to maintain context
+	private Header tempheader;
+        private Relation temprelation;
+        private Task temptask;
+	StringBuffer sb = new StringBuffer();
+	
+	public SAXParserExample(){
+		myrelation = new ArrayList();
+	}
+	
+	public void runExample() {
+		parseDocument();
+		printData();
+	}
+
+	private void parseDocument() {
+		
+		//get a factory
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		try {
+		
+			//get a new instance of parser
+			SAXParser sp = spf.newSAXParser();
+			
+			//parse the file and also register this class for call backs
+			sp.parse("src/exercises-wise13.xml", this);
+			
+		}catch(SAXException se) {
+			se.printStackTrace();
+		}catch(ParserConfigurationException pce) {
+			pce.printStackTrace();
+		}catch (IOException ie) {
+			ie.printStackTrace();
+		}
+	}
+
+	/**
+	 * Iterate through the list and print
+	 * the contents
+	 */
+	private void printData(){
+		
+		System.out.println("No of Relations '" + myrelation.size() + "'.");
+		
+		Iterator it = myrelation.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next().toString());
+		}
+	}
+	
+
+	//Event Handlers
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		//reset
+		sb.setLength(0);
+		if(qName.equalsIgnoreCase("Relation")) {
+			//create a new instance of employee
+			temprelation = new Relation();
+			//temprelation.setType(attributes.getValue("type"));
+		}
+	}
+	
+
+	public void characters(char[] ch, int start, int length) throws SAXException {
+            
+            sb.append(ch,start,length);
+	}
+	
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		if(qName.equalsIgnoreCase("relation")) {
+			//add it to the list
+			myrelation.add(temprelation);
+			
+		}else if (qName.equalsIgnoreCase("intension")) {
+			temprelation.setIntension(sb.toString());
+		}else if (qName.equalsIgnoreCase("tuple")) {
+			temprelation.setTuple(sb.toString());
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		SAXParserExample spe = new SAXParserExample();
+		spe.runExample();
+	}
+	
+}
+
+
+
+
