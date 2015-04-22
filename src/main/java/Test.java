e3908f72e8630f85a64d12df6d001d4c71546c85
Tobias Gruenhagen <tobi.gruenhagen@web.de>
2015-04-22 17:19:01 +0200

    modify xmlfiles

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -26,18 +26,18 @@ public class Test {
         
         sandbox.Task task1 = test.startGame("alchemy-task");
         task1.getMySaxParser().selectFromDb();
-        test.endGame(task1);
+        //test.endGame(task1);
         
         sandbox.Task task2 = test.startGame("exam-wise13");
         task2.getMySaxParser().selectFromDb();
-        test.endGame(task2);
+        //test.endGame(task2);
         
         sandbox.Task task3 = test.startGame("exercises-wise11");
         task3.getMySaxParser().selectFromDb();
-        test.endGame(task3);
+        //test.endGame(task3);
         
         sandbox.Task task4 = test.startGame("exercises-wise13");
         task4.getMySaxParser().selectFromDb();
-        test.endGame(task4);
+        //test.endGame(task4);
     }
 }
