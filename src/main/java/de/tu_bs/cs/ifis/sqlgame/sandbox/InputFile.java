a2e6b70248d7a9832879f2c0e0667f195d47c8e2
Runge93 <tobiasrunge1@web.de>
2015-05-13 13:40:43 +0200

    javadoc

--- src/main/java/de/tu_bs/cs/ifis/sqlgame/sandbox/InputFile.java
+++ src/main/java/de/tu_bs/cs/ifis/sqlgame/sandbox/InputFile.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.tu_bs.cs.ifis.sqlgame.sandbox;
 
 import de.tu_bs.cs.ifis.sqlgame.exception.MySQLAlchemistException;
@@ -11,11 +6,16 @@ import de.tu_bs.cs.ifis.sqlgame.xmlparse.MySAXParser;
 import de.tu_bs.cs.ifis.sqlgame.xmlparse.XMLSyntaxCheck;
 
 /**
+ * Class InputFile
+ *
+ * Create tasks for each task in a input xml-file.
+ * The file is checked and tasks
+ * are only created, if the file is correct.
  *
  * @author Tobias
  */
 public class InputFile {
-    
+
     private String filename;
     private List tasks;
 
@@ -26,17 +26,17 @@ public class InputFile {
     public void setTasks(List tasks) {
         this.tasks = tasks;
     }
-    
-    
+
     /**
      * Constructor InputFile.
-     * 
-     * @param filename String, filenmae without
-     * @throws de.tu_bs.cs.ifis.sqlgame.exception.MySQLAlchemistException throws MySQLAlchemistException
+     *
+     * @param filename String, filename without .xml
+     * @throws de.tu_bs.cs.ifis.sqlgame.exception.MySQLAlchemistException
+     * Exception for the parsing of the document
      */
     public InputFile(String filename) throws MySQLAlchemistException {
         this.filename = filename;
-        
+
         //Make the xml-sructure-check
         XMLSyntaxCheck sych = new XMLSyntaxCheck();
         sych.checkxml(this.filename + ".xml");
