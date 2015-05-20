6c6f590417cf60456823fc8da5db5e077f647d8c
Runge93 <tobiasrunge1@web.de>
2015-05-20 14:01:10 +0200

    input as string

--- src/main/java/de/tu_bs/cs/ifis/sqlgame/sandbox/InputFile.java
+++ src/main/java/de/tu_bs/cs/ifis/sqlgame/sandbox/InputFile.java
@@ -1,9 +1,14 @@
 package de.tu_bs.cs.ifis.sqlgame.sandbox;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import de.tu_bs.cs.ifis.sqlgame.exception.MySQLAlchemistException;
 import java.util.List;
 import de.tu_bs.cs.ifis.sqlgame.xmlparse.MySAXParser;
 import de.tu_bs.cs.ifis.sqlgame.xmlparse.XMLSyntaxCheck;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Class InputFile
@@ -30,20 +35,57 @@ public class InputFile {
     /**
      * Constructor InputFile.
      *
-     * @param filename String, filename without .xml
+     * @param file String, the content of the file
      * @throws de.tu_bs.cs.ifis.sqlgame.exception.MySQLAlchemistException
      * Exception for the parsing of the document
      */
-    public InputFile(String filename) throws MySQLAlchemistException {
-        this.filename = filename;
+    public InputFile(String file) throws MySQLAlchemistException {
+        
+        Config conf = ConfigFactory.load();
+        String path = conf.getString("input.xmlPath");
+
+        String dat = path + "newdata.xml";
+        File newfile = new File(dat);
+        if (checkFile(newfile)) {
+            System.out.println(dat + " erzeugt");
+        }
+        try {
+            FileWriter writer = new FileWriter(newfile);
+            writer.write(file);
+            writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+            throw new MySQLAlchemistException("Fehler beim Erstellen der Datei. ", ex);
+        }
 
         //Make the xml-sructure-check
         XMLSyntaxCheck sych = new XMLSyntaxCheck();
-        sych.checkxml(this.filename + ".xml");
+        sych.checkxml("newdata.xml");
 
         //Parse the xml-file und build the db-tables
         MySAXParser msp = new MySAXParser();
-        msp.parseDocument(this.filename + ".xml");
+        msp.parseDocument("newdata.xml");
         this.tasks = msp.getMyTasks();
+    }
+    
+    /**
+	 * check if the file can be created
+	 * 
+	 * @param file
+	 *            the file that should be created
+	 * @return boolean if it was possible
+	 */
+    private boolean checkFile(File file) throws MySQLAlchemistException {
+        if (file != null) {
+            try {
+                file.createNewFile();
+            } catch (IOException ex) {
+                throw new MySQLAlchemistException("Fehler beim Erstellen der Datei. ", ex);
+            }
+            if (file.isFile() && file.canWrite() && file.canRead()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
