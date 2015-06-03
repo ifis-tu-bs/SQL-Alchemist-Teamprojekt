0f6ffc600db862c5494715b30ceeb3f1e7e796a6
Runge93 <tobiasrunge1@web.de>
2015-06-03 17:31:17 +0200

    javadoc

--- src/main/java/de/tu_bs/cs/ifis/sqlgame/datageneration/DataGenerator.java
+++ src/main/java/de/tu_bs/cs/ifis/sqlgame/datageneration/DataGenerator.java
@@ -392,7 +392,16 @@ public class DataGenerator {
         
         return number;
     }
-
+    /**
+     * Method findAndExecuteFunction
+     * 
+     * This method find the spezific function to generate data from the
+     * given function name and other parameters
+     * @param functionName the name of the fuction
+     * @param params a list of paramters for the function
+     * @return string with the generated data
+     * @throws MySQLAlchemistException Exception, if the user give not the correct parameters
+     */
     private String findAndExecuteFunction(String functionName, ArrayList<String> params) throws MySQLAlchemistException{
         String result = "";
         switch (functionName) {
@@ -591,7 +600,7 @@ public class DataGenerator {
         }
         return result;
     }
-    
+
     private void generateAndExecuteInsertStatement(String tableName, String[] dataRow) throws MySQLAlchemistException {
         String insertedValues = "";
         for (int i = 0; i < dataRow.length; i++) {
@@ -608,30 +617,60 @@ public class DataGenerator {
         );
     }
     
+    /**
+     * Method generateFirstName
+     * 
+     * Generates a random first name
+     * @return String with the name
+     */
     public String generateFirstName() {
         DataFactory df = new DataFactory();
         String result = "'" + df.getFirstName() + "'";
         return result;
     }
-        
+    
+    /**
+     * Method generateLastName
+     * 
+     * Generates a random last name
+     * @return String with the name
+     */   
     public String generateLastName() {
         DataFactory df = new DataFactory();
         String result = "'" + df.getLastName() + "'";
         return result;
     }
     
+    /**
+     * Method generateFullName
+     * 
+     * Generates a random first and last name
+     * @return String with the name
+     */  
     public String generateFullName() {
         DataFactory df = new DataFactory();
         String result = "'" + df.getFirstName() + " " + df.getLastName() + "'";
         return result;
     }
     
+    /**
+     * Method generateInteger
+     * 
+     * Generates a random integer
+     * @return String with the integer
+     */
     public String generateInteger() {
         Random r = new Random();
         String result = "" + r.nextInt();
         return result;
     }
     
+    /**
+     * Method generateIntegerPos
+     * 
+     * Generates a random positive integer
+     * @return String with the integer
+     */
     public String generateIntegerPos() {
         Random r = new Random();
         int i = r.nextInt();
@@ -642,18 +681,37 @@ public class DataGenerator {
         return result;
     }
     
+    /**
+     * Method generateDouble
+     * 
+     * Generates a random floating-point number
+     * @return String with the number
+     */
     public String generateDouble() {
         Random r = new Random();
         String result = "" + r.nextDouble();
         return result;
     }
     
+    /**
+     * Method generateString
+     * 
+     * Generates a random string
+     * @param length the length of the string
+     * @return String with the string
+     */
     public String generateString(int length) {
         DataFactory df = new DataFactory();
         String result = "'" + df.getRandomChars(length) + "'";
         return result;
     }
     
+    /**
+     * Method generateDate
+     * 
+     * Generates a random date "yyyy-MM-dd"
+     * @return String with the date
+     */
     public String generateDate() {
         DataFactory df = new DataFactory();
         String date = new SimpleDateFormat("yyyy-MM-dd").format(df.getBirthDate());
@@ -661,72 +719,154 @@ public class DataGenerator {
         return result;
     }
     
+    /**
+     * Method generateBusinessName
+     * 
+     * Generates a random business name
+     * @return String with the name
+     */
     public String generateBusinessName() {
         DataFactory df = new DataFactory();
         String result = "'" + df.getBusinessName() + "'";
         return result;
     }
     
+    /**
+     * Method generateStreetName
+     * 
+     * Generates a random street name
+     * @return String with the name
+     */
     public String generateStreetName() {
         DataFactory df = new DataFactory();
         String result = "'" + df.getStreetName() + "'";
         return result;
     }
     
+    /**
+     * Method generateCity
+     * 
+     * Generates a random city name
+     * @return String with the city
+     */
     public String generateCity() {
         DataFactory df = new DataFactory();
         String result = "'" + df.getCity() + "'";
         return result;
     }
     
+    /**
+     * Method generateAdress
+     * 
+     * Generates a random adress
+     * @return String with the adress
+     */
     public String generateAdress() {
         DataFactory df = new DataFactory();
         String result = "'" + df.getAddress() + "'";
         return result;
     }
     
+    /**
+     * Method generateEmail
+     * 
+     * Generates a random email adress
+     * @return String with the email
+     */
     public String generateEmail() {
         DataFactory df = new DataFactory();
         String result = "'" + df.getEmailAddress() + "'";
         return result;
     }
     
+    /**
+     * Method generateMinInteger
+     * 
+     * Generates an integer greater than the parameter
+     * @param min the minimum integer
+     * @return String with the integer
+     */
     public String generateMinInteger(int min) {
         DataFactory df = new DataFactory();
         String result = "" + df.getNumberBetween(min, 1000000);
         return result;
     }
     
+    /**
+     * Method generateMinDouble
+     * 
+     * Generates a double greater than the parameter
+     * @param min the minimum double
+     * @return String with the double
+     */
     public String generateMinDouble(int min) {
         DataFactory df = new DataFactory();
         String result = "" + df.getNumberBetween(min, 1000000) + "." + df.getNumberBetween(0, 99);
         return result;
     }
-        
+    
+    /**
+     * Method generateMaxInteger
+     * 
+     * Generates an integer less than the parameter
+     * @param max the maximum integer
+     * @return String with the integer
+     */
     public String generateMaxInteger(int max) {
         DataFactory df = new DataFactory();
         String result = "" + df.getNumberUpTo(max);
         return result;
     }
     
+    /**
+     * Method generateMaxDouble
+     * 
+     * Generates a double less than the parameter
+     * @param max the maximum double
+     * @return String with the double
+     */
     public String generateMaxDouble(int max) {
         DataFactory df = new DataFactory();
         String result = "" + df.getNumberUpTo(max) + "." + df.getNumberBetween(0, 99);
         return result;
     }
     
+    /**
+     * Method generateBetweenInteger
+     * 
+     * Generates an integer between the parameter
+     * @param min the minimum integer
+     * @param max the maximum integer
+     * @return String with the integer
+     */
     public String generateBetweenInteger(int min, int max) {
         DataFactory df = new DataFactory();
         String result = "" + df.getNumberBetween(min, max);
         return result;
     }
     
+    /**
+     * Method generateBetweenDouble
+     * 
+     * Generates a double between the parameter
+     * @param min the minimum double
+     * @param max the maximum double
+     * @return String with the double
+     */
     public String generateBetweenDouble(int min, int max) {
         DataFactory df = new DataFactory();
         String result = "" + df.getNumberBetween(min, max-1) + "." + df.getNumberBetween(0, 99);
         return result;
     }
     
+    /**
+     * Method generateGaussInt
+     * 
+     * Generates an integer with the Gaussian distribution
+     * @param median the median of the Gaussian distribution
+     * @param sd the standard deviation	of the Gaussian distribution
+     * @return String with the integer
+     */
     public String generateGaussInt(double median, double sd) {
         Random r = new Random();
         double d =  median + r.nextGaussian() * sd;
@@ -735,6 +875,14 @@ public class DataGenerator {
         return result;
     }
     
+    /**
+     * Method generateGaussDouble
+     * 
+     * Generates a double with the Gaussian distribution
+     * @param median the median of the Gaussian distribution
+     * @param sd the standard deviation	of the Gaussian distribution
+     * @return String with the double
+     */
     public String generateGaussDouble(double median, double sd) {
         Random r = new Random();
         double d =  median + r.nextGaussian() * sd;
@@ -742,11 +890,28 @@ public class DataGenerator {
         return result;
     }
     
+    /**
+     * Method generateList
+     * 
+     * Generates an integer of the list
+     * @param start the startelement
+     * @return String with the number
+     */
     public String generateList(int start) {
         String result = "" + start;
         return result;
     }
     
+    /**
+     * Method generateCustomData
+     * 
+     * Generates a random string with our txt-files
+     * @param metaData the name of the txt-file
+     * @param random the percentage of how often the value or the default is chosen
+     * @param defaultValue the defaultvalue
+     * @return
+     * @throws MySQLAlchemistException 
+     */
     public String generateCustomData(String metaData, int random, String defaultValue) throws MySQLAlchemistException {
         try {
             DataFactory df = new DataFactory();
